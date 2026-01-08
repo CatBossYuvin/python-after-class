33ba0040f0b60e88ad2dacc1ebfdb9d48c874e21
CatBossYuvin <yuvin054@gmail.com>
2026-01-09 09:49:23 +1300

    initial commit

---
+++ sq root.py
@@ -0,0 +1,12 @@
+print("Enter enter square root:")
+math=int(input("sqaure root: "))
+print("Enter enter  another square root:")
+Python=int(input("square root:"))
+
+
+sum=math*Python
+
+print("Sum of these two numbers are:",sum)
+
+
+
