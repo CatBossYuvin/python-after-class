a272393aaf0888ac01c833a9d9d83abe1e4a2296
CatBossYuvin <yuvin054@gmail.com>
2026-01-09 13:58:24 +1300

    initial commit

---
+++ HOT_OR_COLD.py
@@ -0,0 +1,6 @@
+print("Welcome to the hot or cold outfit suggester!,Enter 1 if you are feeling hot or 2 if you are feeling cold")
+feel=int(input("How are you feeling today number: "))
+if feel==1:
+    print("you are feeling hot? What about a nice pair of shorts and a shirt and a icecream")
+else:
+    print("you are feeling cold? How about a nice warm sweater and a jacket with a hot chocolate")
